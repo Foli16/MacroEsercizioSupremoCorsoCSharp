2ab0b05765e1bce666e4578006a9bc1fed1e024f
fcard-dev <fabrizia.cardinale@gmail.com>
2026-03-06 17:22:43 +0100

    bozza

--- obj/Debug/net10.0/RepoGruppo.AssemblyInfo.cs
+++ obj/Debug/net10.0/RepoGruppo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepoGruppo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8976eecb1cd38649c135bf3fbbf61e41712451c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2ed958d09d51e91db740060551deec4e16cfca9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepoGruppo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepoGruppo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
